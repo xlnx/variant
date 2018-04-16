7516ade805225acc8b5c8f7e8a3051540f974749
xlnx <qwq@koishi.top>
2018-04-16 23:20:34 +0800

    modified variant, fix move/copy bugs on void type.

--- test_variant.cpp
+++ test_variant.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-	variant<int, double> var = 3, v = 1.5;
-	var.make_switch<int, double>(
+	variant<int, double, string> var = 3, v = 1.5, c = string("asd");
+	var.make_match<int, double>(
 		[](int& n){ std::cout << n << std::endl; },
 		[](double&){}
 	);
